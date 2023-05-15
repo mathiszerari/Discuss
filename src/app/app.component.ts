f71e01ec0d2a058bd2dbb473e81ae92c648a4881
Mathis Zerari <mathiszerari@icloud.com>
2023-05-15 21:04:12 +0200

    modulaire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-<body class="bg-gray-900 h-screen">
-  
   <div class="mx-40 pt-10 h-10 min-h-screen">
-  <app-title></app-title>
+    <app-title></app-title>
 
     <app-question></app-question>
 
     <app-input></app-input>
 
-    <app-reponses></app-reponses>
-
+    <app-reponses></app-reponses> 
   </div>
-</body>
   `,
   styles: []
 })
