eb0110a4d1494a13f1f98254652ddc292fefeb4f
Mathis Zerari <mathiszerari@icloud.com>
2023-07-01 19:08:07 +0200

    new folders in the component folder

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -8,9 +8,9 @@ import { ConceptComponent } from './component/concept/concept/concept.component'
 import { ReponsesComponent } from './component/home/reponses/reponses.component';
 import { InputComponent } from './component/home/input/input.component';
 import { TitleComponent } from './component/common/title/title.component';
-import { FooterComponent } from './component/home/footer/footer.component';
+import { FooterComponent } from './component/concept/footer/footer.component';
 import { HomeComponent } from './component/home/home/home.component';
-import { FooterRedirectHomeComponent } from './component/home/footer-redirect-home/footer-redirect-home.component';
+import { FooterRedirectHomeComponent } from './component/concept/footer-redirect-home/footer-redirect-home.component';
 import { ConceptTextComponent } from './component/concept/concept-text/concept-text.component';
 import { ReplyService } from './services/reply.service';
 import { ConnexionComponent } from './component/home/connexion/connexion.component';
